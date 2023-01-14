facbb4a32d671f91c74cae3400efa326a00edbc6
Andrew Oseen <amovfx@protonmail.com>
2023-01-14 15:49:04 -0700

    fix(reivew): hit majority of notes
    Removed asset buttons
    cleaned up translations
    Renamed OpenMintAssetModal to MintAssetButton

--- src/components/designer/taro/actions/index.ts
+++ src/components/designer/taro/actions/index.ts
@@ -1,6 +1,4 @@
 export { default as MintAssetButton } from './MintAssetButton';
-export { default as OpenSendAssetModal } from './OpenSendAssetModal';
 export { default as MintAssetModal } from './MintAssetModal';
-export { default as SendAssetModal } from './SendAssetModal';
 export { default as NewAddressModal } from './NewAddressModal';
 export { default as NewAddressButton } from './NewAddressButton';
